03131c80412c0e11bfd6a5fbce92f3cb505b2b6f
John Shaffer <jdsha@proton.me>
2023-01-07 13:26:27 -0600

    Update arguments to new babashka.process convention
    See https://github.com/babashka/process/issues/95

--- src/srvc/server/review.clj
+++ src/srvc/server/review.clj
@@ -13,8 +13,11 @@
    :out :inherit
    :shutdown p/destroy-tree})
 
-(defn process [args & [opts]]
-  (p/process args (merge default-opts opts)))
+(defn process [& args]
+  (let [[maybe-opts & more] args]
+    (if (map? maybe-opts)
+      (apply p/process (merge default-opts maybe-opts) more)
+      (apply p/process args))))
 
 (defrecord TailListener [f g]
   TailerListener
@@ -52,8 +55,8 @@
     (when (fs/exists? db)
       (fs/copy db sink))
     {:process (process
-               ["sr" "--config" (str config-file) "review" flow-name]
-               {:dir (str dir)})
+               {:dir (str dir)}
+               "sr" "--config" (str config-file) "review" flow-name)
      :sink-path sink
      :sink-thread (-> (tailer add-events! tail-exception! sink) start-daemon)
      :temp-dir temp-dir}))
