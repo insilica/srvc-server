e4a35c6786ba46d96ad4c066f9c3ff9773919287
John Shaffer <jdsha@proton.me>
2023-01-25 14:36:36 -0600

    Upgrade deps

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    srvc.url = "github:insilica/rs-srvc";
+    srvc = {
+      url = "github:insilica/rs-srvc";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
   outputs = { self, nixpkgs, flake-utils, srvc, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
